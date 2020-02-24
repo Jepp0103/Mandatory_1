9c75f81b6554a213b1fc131b9988b497b0f4bbed
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-02-24 15:08:02 +0100

    ...

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-//the way to import a package from node modules 
+//The way to import a package from node modules 
 var express = require("express");
 var app = express();
 const request = require("request");
 
-//getting html files
-app.get("/main", (req, res) => {
-   return res.sendfile(__dirname + '/resources/main.html');
+//Getting html files
+app.get("/arraysAndLoops", (req, res) => {
+    return res.sendfile(__dirname + '/resources/arraysAndLoops.html');
+ });
+
+app.get("/basicsStringsObjects", (req, res) => {
+   return res.sendfile(__dirname + '/resources/basicsStringsObjects.html');
 });
 
-app.get("/documentation", (req, res) => {
-   return res.sendfile(__dirname + '/resources/docOne.html');
-});
+ app.get("/commandsAndTools", (req, res) => {
+    return res.sendfile(__dirname + '/resources/commandsAndTools.html');
+ });
 
-//listens for port number to a given server
+ app.get("/jQueryAndRouting", (req, res) => {
+    return res.sendfile(__dirname + '/resources/jQueryAndRouting.html');
+ });
+
+ app.get("/", (req, res) => {
+    return res.sendfile(__dirname + '/resources/main.html');
+ });
+
+//Listens for port number to a given server
 var port = 8080;
 app.listen(port, error => {
     console.log(error);
