ff6d5452e44e1770888c1fdde51ba288005fcee3
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-03-03 20:23:59 +0100

    :..

--- jQueries/commandsAndToolsjQuery.js
+++ jQueries/commandsAndToolsjQuery.js
@@ -1,5 +1,6 @@
 //jQuery hide and show buttons for the commandsAndTools page
-  //Node section
+  
+//Node paragraph
   var nodeButton = "#nodeButton"
   var nodeInfo = "#nodeInfo, #nodeRunningInfo"
   $(nodeButton).css("background-color", "#ff3333");
@@ -24,9 +25,10 @@
       nodeClickCount++;
   });
 
-  //Express section
+  //Express paragraph
   var expressButton = "#expressButton"
   var expressInfo = "#expressInfo, #setupExpressInfo"
+
   $(expressButton).css("background-color", "#ff3333");
   var expressClickCount = 0;
   $(expressButton).text("Hide express paragraph");
@@ -48,3 +50,55 @@
       
       expressClickCount++;
   });
+
+  //nodemon paragraph
+  var nodemonButton = "#nodemonButton"
+  var nodemonInfo = "#nodemonInfo, #setupnodemonInfo"
+
+  $(nodemonButton).css("background-color", "#ff3333");
+  var nodemonClickCount = 0;
+  $(nodemonButton).text("Hide nodemon paragraph");
+
+  $(nodemonButton).click(() => {
+      console.log(nodemonClickCount);
+
+      if(nodemonClickCount % 2 == 0) {
+        $(nodemonInfo).toggle();
+        $(nodemonButton).text("Show nodemon paragraph");
+        $(nodemonButton).css("background-color", "Chartreuse");
+      }
+
+      else {
+        $(nodemonInfo).toggle();
+        $(nodemonButton).text("Hide nodemon paragraph");
+        $(nodemonButton).css("background-color", "#ff3333");
+      }
+      
+      nodemonClickCount++;
+  });
+
+    //budo paragraph
+    var budoButton = "#budoButton"
+    var budoInfo = "#budoInfo, #setupBudoInfo"
+  
+    $(budoButton).css("background-color", "#ff3333");
+    var budoClickCount = 0;
+    $(budoButton).text("Hide nodemon paragraph");
+  
+    $(budoButton).click(() => {
+        console.log(budoClickCount);
+  
+        if(budoClickCount % 2 == 0) {
+          $(budoInfo).toggle();
+          $(budoButton).text("Show nodemon paragraph");
+          $(budoButton).css("background-color", "Chartreuse");
+        }
+  
+        else {
+          $(budoInfo).toggle();
+          $(budoButton).text("Hide nodemon paragraph");
+          $(budoButton).css("background-color", "#ff3333");
+        }
+        
+        budoClickCount++;
+    });
