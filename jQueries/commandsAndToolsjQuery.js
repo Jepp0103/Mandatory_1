5ebf37284f9acc83ffcdb8316a16a7345fc27ce6
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-03-13 20:53:12 +0100

    ...

--- jQueries/commandsAndToolsjQuery.js
+++ jQueries/commandsAndToolsjQuery.js
@@ -25,29 +25,29 @@
         nodeClickCount++;
     });
 
-     //Routing paragraph
-     var routingButton = "#routingButton"
-     var routingInfo = "#routingInfo"
-     $(routingButton).css("background-color", "#ff3333");
-     var routingClickCount = 0;
-     $(routingButton).text("Hide routing paragraph");
+     //npm paragraph
+     var npmButton = "#npmButton"
+     var npmInfo = "#npmInfo, #npmRunInfo, #npmInitInfo"
+     $(npmButton).css("background-color", "#ff3333");
+     var npmClickCount = 0;
+     $(npmButton).text("Hide npm paragraph");
  
-     $(routingButton).click(() => {
-         console.log(routingClickCount);
+     $(npmButton).click(() => {
+         console.log(npmClickCount);
  
-         if (routingClickCount % 2 == 0) {
-           $(routingInfo).toggle();
-           $(routingButton).text("Show routing paragraph");
-           $(routingButton).css("background-color", "Chartreuse");
+         if (npmClickCount % 2 == 0) {
+           $(npmInfo).toggle();
+           $(npmButton).text("Show npm paragraph");
+           $(npmButton).css("background-color", "Chartreuse");
          }
  
          else {
-           $(routingInfo).toggle();
-           $(routingButton).text("Hide routing paragraph");
-           $(routingButton).css("background-color", "#ff3333");
+           $(npmInfo).toggle();
+           $(npmButton).text("Hide npm paragraph");
+           $(npmButton).css("background-color", "#ff3333");
          }
          
-         routingClickCount++;
+         npmClickCount++;
      }); 
 
     //Express paragraph
