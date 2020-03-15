48218320fc60dfe5cc3b6259b9906f1d3a8521a0
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-03-15 16:01:25 +0100

    Mandatory 1 done

--- jQueries/routingAndRequestsjQuery.js
+++ jQueries/routingAndRequestsjQuery.js
@@ -1,24 +1,50 @@
-//Routing paragraph
-var routingButton = "#routingButton"
-var routingInfo = "#routingInfo"
-$(routingButton).css("background-color", "#ff3333");
-var routingClickCount = 0;
-$(routingButton).text("Hide routing paragraph");
+//Hiding the different paragraphs
+    //Routing paragraph
+    var routingButton = "#routingButton"
+    var routingInfo = "#routingInfo"
+    $(routingButton).css("background-color", "#ff3333");
+    var routingClickCount = 0;
+    $(routingButton).text("Hide routing paragraph");
 
-$(routingButton).click(() => {
-    console.log(routingClickCount);
+    $(routingButton).click(() => {
+        console.log(routingClickCount);
 
-    if (routingClickCount % 2 == 0) {
-      $(routingInfo).toggle();
-      $(routingButton).text("Show routing paragraph");
-      $(routingButton).css("background-color", "Chartreuse");
-    }
+        if (routingClickCount % 2 == 0) {
+          $(routingInfo).toggle();
+          $(routingButton).text("Show routing paragraph");
+          $(routingButton).css("background-color", "Chartreuse");
+        }
 
-    else {
-      $(routingInfo).toggle();
-      $(routingButton).text("Hide routing paragraph");
-      $(routingButton).css("background-color", "#ff3333");
-    }
-    
-    routingClickCount++;
-}); 
+        else {
+          $(routingInfo).toggle();
+          $(routingButton).text("Hide routing paragraph");
+          $(routingButton).css("background-color", "#ff3333");
+        }
+        
+        routingClickCount++;
+    }); 
+
+    //Requests paragraph
+    var requestsButton = "#requestsButton"
+    var requestsInfo = "#requestsInfo"
+    $(requestsButton).css("background-color", "#ff3333");
+    var requestsClickCount = 0;
+    $(requestsButton).text("Hide requests paragraph");
+
+    $(requestsButton).click(() => {
+        console.log(requestsClickCount);
+
+        if (requestsClickCount % 2 == 0) {
+          $(requestsInfo).toggle();
+          $(requestsButton).text("Show requests paragraph");
+          $(requestsButton).css("background-color", "Chartreuse");
+        }
+
+        else {
+          $(requestsInfo).toggle();
+          $(requestsButton).text("Hide requests paragraph");
+          $(requestsButton).css("background-color", "#ff3333");
+        }
+        
+        requestsClickCount++;
+    }); 
